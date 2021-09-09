183bf45340cc5a5edc5353c4b46f96d413914db7
_loff <lachixd@gmail.com>
2021-09-09 20:42:12 +1000

    Add files via upload

---
+++ XDTBACKEND.py
@@ -0,0 +1,275 @@
+"""XDTBACKEND.py: The workhorse of XDOCKTOOL which is incapable of doing things by itself."""
+
+__author__ = "Lachlan Angus"
+__copyright__ = "Copyright 2021, Lachlan Angus"
+
+import datetime
+import email
+import json
+import os
+import platform
+import subprocess
+
+from bs4 import BeautifulSoup
+from fpdf import FPDF
+from tabulate import tabulate
+
+manifests = []
+manifest_file = "manifests.json"
+
+user_settings = {
+    "show_all_articles": False,
+    "open_on_save": True
+}
+
+
+class Manifest:
+    def __init__(self, manifest_id):
+        self.manifest_id = manifest_id
+        self.ssccs = []
+
+    def __repr__(self):
+        return self.manifest_id
+
+    def export(self):
+        sscc_list = []
+        for sscc in self.ssccs:
+            sscc_list.append(sscc.export())
+        return {"Manifest ID": self.manifest_id, "SSCCs": sscc_list}
+
+    def __eq__(self, other):
+        return int(self.manifest_id) == int(other.manifest_id)
+
+    def __lt__(self, other):
+        return int(self.manifest_id) < int(other.manifest_id)
+
+
+class SSCC:
+    def __init__(self, sscc):
+        self.sscc = sscc
+        self.articles = []
+        self.short_sscc = sscc[-4:]
+        self.is_HR = False
+
+    def __eq__(self, other):
+        return self.short_sscc == other.short_sscc
+
+    def __lt__(self, other):
+        return self.short_sscc < other.short_sscc
+
+    def __repr__(self):
+        return str(self.sscc)
+
+    def export(self):
+        article_list = []
+        for article in self.articles:
+            article_list.append(article.export())
+        return {"SSCC": self.sscc, "is_HR": self.is_HR, "Articles": article_list}
+
+    def check_HR(self):
+        for article in self.articles:
+            if article.is_HR:
+                self.is_HR = True
+
+    def HR_repr(self):
+        if self.is_HR:
+            return "(HR)"
+        else:
+            return "    "
+
+    def article_repr(self):
+        if len(self.articles) > 1:
+            return "Multiple"
+        else:
+            return self.articles[0].code
+
+    def desc_repr(self):
+        if user_settings["show_all_articles"]:
+            if len(self.articles) > 1:
+                string = ""
+                for article in self.articles:
+                    string += article.code + "\n"
+                return string
+            else:
+                return self.articles[0].desc
+        else:
+            if len(self.articles) > 1:
+                return "Lines contained in SSCC:"
+            else:
+                return self.articles[0].desc
+
+    def qty_repr(self):
+        if user_settings["show_all_articles"]:
+            if len(self.articles) > 1:
+                string = ""
+                for article in self.articles:
+                    string += article.qty + "\n"
+                return string
+            else:
+                return self.articles[0].qty
+        else:
+            if len(self.articles) > 1:
+                return len(self.articles)
+            else:
+                return self.articles[0].qty
+
+
+class Article:
+    def __init__(self, code, desc, gtin, qty):
+        self.code = code
+        self.desc = desc
+        self.gtin = gtin
+        self.qty = qty
+        self.is_HR = False
+
+    def __repr__(self):
+        return self.code
+
+    def export(self):
+        return {"Code": self.code, "Desc": self.desc, "GTIN": self.gtin, "QTY": self.qty, "is_HR": self.is_HR}
+
+
+# noinspection PyBroadException
+def junk_check(var):
+    try:
+        if len(var[1][0]) > 4:
+            int(var[1][0])
+            return True
+    except:
+        return False
+
+
+# noinspection PyBroadException
+def mhtml_importer(file_path):
+    # Import mhtml
+    with open(file_path, 'r') as source:
+        content = email.message_from_file(source)
+        for unit in content.walk():
+            if unit.get_content_type() == "text/html":
+                raw_file = BeautifulSoup(unit.get_payload(decode=False), 'html.parser')
+
+    # Populate variables
+    new_manifest = Manifest("000000")
+    for row in raw_file.findAll('tr'):
+        new_entry = []
+        for element in row.findAll('td'):
+            new_entry.append(element.contents)
+
+        if junk_check(new_entry):  # This is the gatekeeper of junk entries
+            # manifest[0], sscc[1], count[2], article[3], desc[4], handling_unit_uom[5], gtin[6], qty[7] -> Order
+            # of information
+            entry_placed = False
+            new_article = Article(new_entry[3][0], new_entry[4][0], new_entry[6][0], new_entry[7][0])
+            for exist_sscc in new_manifest.ssccs:
+                if exist_sscc.sscc == new_entry[1][0]:
+                    exist_sscc.articles.append(new_article)
+                    entry_placed = True
+
+            if not entry_placed:
+                new_sscc = SSCC(new_entry[1][0])
+                new_sscc.articles.append(new_article)
+                new_manifest.ssccs.append(new_sscc)
+
+            if new_manifest.manifest_id == "000000":
+                new_manifest.manifest_id = new_entry[0][0]
+
+    manifests.append(new_manifest)
+    return new_manifest.manifest_id
+
+
+def json_load():
+    # Load the JSON
+    global manifests
+    manifests.clear()
+    if not os.path.isfile(manifest_file):
+        with open(manifest_file, "a+") as file:
+            json.dump([], file)
+    with open(manifest_file, "r") as file:
+        manifest_in = json.load(file)
+
+    # Convert JSON back into manifest array
+    for manifest in manifest_in:
+        new_manifest = Manifest(manifest["Manifest ID"])
+        for sscc in manifest["SSCCs"]:
+            new_sscc = SSCC(sscc["SSCC"])
+            new_sscc.is_HR = sscc["is_HR"]
+            for article in sscc["Articles"]:
+                new_sscc.articles.append(Article(article["Code"], article["Desc"], article["GTIN"], article["QTY"]))
+            new_manifest.ssccs.append(new_sscc)
+        manifests.append(new_manifest)
+
+
+def json_save():
+    # Save manifests array to JSON
+    json_out = []
+    for manifest in manifests:
+        json_out.append(manifest.export())
+    os.rename(manifest_file, (manifest_file + ".bak"))
+    with open(manifest_file, "a+") as file:
+        json.dump(json_out, file, indent=4)
+    os.remove(manifest_file + ".bak")
+
+
+def do_HR_check():
+    # TODO HR Article register
+    for manifest in manifests:
+        for sscc in manifest.ssccs:
+            sscc.check_HR()
+
+
+def generate_pdf(manifest, pdf_location):
+    # Create file and header
+    pdf_file = FPDF("P", "mm", "A4")
+    pdf_file.add_page()
+    pdf_file.set_font('Courier', '', 14)
+    pdf_file.set_title(manifest.manifest_id)
+    title_text = "Manifest: " + manifest.manifest_id + " - Printed on: " + str(datetime.date.today())
+    pdf_file.cell(w=200, h=12, txt=title_text, ln=1, align="L")
+
+    # Make the table for the PDF
+    # TODO Multiple modes
+    do_HR_check()
+    pdf_file.set_font('Courier', '', 9)
+    pdf_file.set_fill_color(220)
+    tb_content = [["Article", "Description".ljust(30), "Qty", "SSCC", "HR?", "Short", "C"]]
+
+    for sscc in sorted(manifest.ssccs):
+        tb_content.append([sscc.article_repr(), sscc.desc_repr(), sscc.qty_repr(), sscc.sscc, sscc.HR_repr(), sscc.short_sscc, "[    ]"])
+
+    cell_text = (tabulate(tb_content, headers="firstrow", tablefmt="simple")).split("\n")
+
+    # Darken for HR items
+    for row in cell_text:
+        if "(HR)" in row:
+            pdf_file.cell(w=0, h=5, txt=row, ln=1, align="L", fill=True, border=1)
+        else:
+            pdf_file.cell(w=0, h=5, txt=row, ln=1, align="L", border=1)
+
+    # Actually save the pdf
+    pdf_file.output(pdf_location, 'F')
+
+    # Open the pdf if user has requested
+    if user_settings["open_on_save"]:
+        if platform.system() == 'Darwin':  # macOS
+            subprocess.call(('open', pdf_location))
+        elif platform.system() == 'Windows':  # Windows
+            os.startfile(pdf_location)
+        else:  # linux variants
+            subprocess.call('xdg-open', pdf_location)
+
+
+def format_preview(selected_manifest):
+    manifest = get_manifest_from_id(selected_manifest)
+    tb_content = [["Article", "Description".ljust(30), "Qty", "SSCC", "HR?", "Short", "C"]]
+    for sscc in sorted(manifest.ssccs):
+        tb_content.append(
+            [sscc.article_repr(), sscc.desc_repr(), sscc.qty_repr(), sscc.sscc, sscc.HR_repr(), sscc.short_sscc,
+             "[  ]"])
+    return tabulate(tb_content, headers="firstrow", tablefmt="fancy_grid")
+
+
+def get_manifest_from_id(manifest_id):
+    for manifest in manifests:
+        if manifest.manifest_id == manifest_id:
+            return manifest
+    return "000000"
---
+++ XDTFRONTEND.py
@@ -0,0 +1,201 @@
+"""XDTFRONTEND.py: The pants-wearing part of XDOCKTOOL which tells backend what to do"""
+
+__author__ = "Lachlan Angus"
+__copyright__ = "Copyright 2021, Lachlan Angus"
+
+from tkinter import *
+from tkinter import ttk, filedialog
+
+import XDTBACKEND as backend
+
+selected_manifest = ""
+backend.json_load()
+user_settings = backend.user_settings
+
+
+# noinspection PyBroadException
+def open_mhtml():
+    try:
+        mhtml_location = filedialog.askopenfilename(filetypes=[("SAP Manifest", ".MHTML")])
+        imported_manifest_id = backend.mhtml_importer(mhtml_location)
+        interface_update()
+
+        combo_select_manifest.set(imported_manifest_id)
+        interface_update()
+    except:
+        pass
+
+
+def generate_pdf():
+    manifest = backend.get_manifest_from_id(selected_manifest)
+    save_location = filedialog.asksaveasfilename(filetypes=[("PDF Document", ".pdf")],
+                                                 initialfile=str(manifest.manifest_id + ".pdf"))
+    if len(save_location) > 0:
+        backend.generate_pdf(manifest, save_location)
+
+
+def interface_update(*event):
+    global selected_manifest
+    selected_manifest = combo_select_manifest.get()
+    combo_select_manifest["values"] = sorted(backend.manifests)
+
+    user_settings["show_all_articles"] = bool(var_show_all_articles.get())
+    user_settings["open_on_save"] = bool(var_open_on_save.get())
+    backend.user_settings = user_settings
+
+    if len(selected_manifest) > 0:
+        text_preview['state'] = 'normal'
+        text_preview.delete(1.0, END)
+        text_preview.insert(INSERT, backend.format_preview(selected_manifest))
+        text_preview['state'] = 'disabled'
+
+        preview_text.configure(text="Manifest " + selected_manifest + " preview:")
+
+
+def HR_manager():
+    if len(selected_manifest) > 0:
+
+        manifest = backend.get_manifest_from_id(selected_manifest)
+
+        hr_manager_window = Toplevel(root)
+        hr_manager_window.wait_visibility()
+        hr_manager_window.grab_set()
+        hr_manager_window.title("Select HR entries")
+        hr_manager_window.geometry("300x600")
+        hr_manager_window.rowconfigure(0, weight=1)
+        hr_manager_window.columnconfigure(0, weight=1)
+
+        dialog_frame = ttk.Frame(hr_manager_window)
+        dialog_frame.grid(sticky=(N, S, E, W))
+        dialog_frame.rowconfigure(0, weight=1)
+        dialog_frame.columnconfigure(0, weight=1)
+
+        # Populate list
+        def last_four(string):
+            return str(string)[-4:]
+
+        hr_sscc_list = []
+        for sscc in manifest.ssccs:
+            hr_sscc_list.append(sscc.sscc[:-4] + " " + last_four(sscc.sscc))
+
+        hr_listbox_content = StringVar()
+        hr_sscc_list = sorted(list(dict.fromkeys(hr_sscc_list)), key=last_four)
+        hr_listbox = Listbox(dialog_frame, height=36, width=30, listvariable=hr_listbox_content, selectmode="multiple")
+        hr_listbox.grid(column=0, row=0)
+        hr_listbox.configure(justify=RIGHT)
+        hr_listbox_content.set(hr_sscc_list)
+
+        listbox_scroll = Scrollbar(dialog_frame, command=hr_listbox.yview)
+        listbox_scroll.grid(column=1, row=0, sticky=(N, S))
+        hr_listbox['yscrollcommand'] = listbox_scroll.set
+
+        # Select lines already marked as HR
+        # TODO this.
+        for sscc in manifest.ssccs:
+            if sscc.is_HR:
+                # thing
+                pass
+
+        # Close HR manager and save changes to array
+        def save_hr():
+            for config_sscc in manifest.ssccs:
+                config_sscc.is_HR = False
+                # TODO can add article checks here
+
+            # Get selection
+            listbox_selected = []
+            for index in hr_listbox.curselection():
+                listbox_selected.append(str(hr_listbox.get(index)).replace(" ", ""))
+
+            # Apply HR tags
+            for selection in listbox_selected:
+                for config_sscc in manifest.ssccs:
+                    if config_sscc.sscc == selection:
+                        config_sscc.is_HR = True
+
+            backend.manifests.remove(backend.get_manifest_from_id(selected_manifest))
+            backend.manifests.append(manifest)
+            backend.json_save()
+            interface_update()
+            hr_manager_window.grab_release()
+            hr_manager_window.destroy()
+
+        dialog_close_button = Button(dialog_frame, text="Finished", command=save_hr)
+        dialog_close_button.grid(column=0, row=1)
+
+
+def exit_app():
+    backend.json_save()
+    root.destroy()
+
+
+# Root Window
+root = Tk()
+root.title("X-Dock Manager")
+root.iconbitmap("XDMGR.ico")
+root.geometry("1024x768")
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+root.wm_protocol("WM_DELETE_WINDOW", lambda: exit_app())
+
+# Main widget container
+main_container = ttk.Frame(root)
+main_container.grid(column=0, row=0, sticky=(N, S, E, W))
+main_container.columnconfigure(0, weight=1)
+main_container.rowconfigure(2, weight=1)
+
+# Control Panel frame
+control_panel = ttk.Frame(main_container)
+control_panel.grid(column=0, row=0, sticky=(N, W, E, S))
+
+combo_select_manifest = ttk.Combobox(control_panel, )
+combo_select_manifest.grid(column=0, row=0)
+combo_select_manifest["values"] = sorted(backend.manifests)
+combo_select_manifest.bind("<<ComboboxSelected>>", interface_update)
+combo_select_manifest.grid_configure(padx=10, pady=5)
+
+button_open = Button(control_panel, text="Import MHTML", command=open_mhtml)
+button_open.grid(column=1, row=0)
+
+button_set_hr = Button(control_panel, text="HR Config", command=HR_manager)
+button_set_hr.grid(column=2, row=0)
+
+button_export_pdf = Button(control_panel, text="Save PDF", command=generate_pdf)
+button_export_pdf.grid(column=3, row=0)
+
+var_show_all_articles = IntVar()
+check_show_all_articles = Checkbutton(control_panel, text="Show all articles", variable=var_show_all_articles,
+                                      command=interface_update)
+check_show_all_articles.grid(column=10, row=0)
+check_show_all_articles.deselect()
+
+var_open_on_save = IntVar()
+check_open_on_save = Checkbutton(control_panel, text="Open on save", variable=var_open_on_save,
+                                 command=interface_update)
+check_open_on_save.grid(column=11, row=0)
+check_open_on_save.select()
+
+# Preview title
+preview_text = Label(main_container, text="Manifest preview:")
+preview_text.grid(column=0, row=1)
+
+# Manifest Preview
+preview_frame = ttk.Frame(main_container)
+preview_frame.grid(column=0, row=2, sticky=(N, W, E, S))
+preview_frame.columnconfigure(0, weight=1)
+preview_frame.rowconfigure(0, weight=1)
+
+text_preview = Text(preview_frame)
+text_preview.grid(column=0, row=0, sticky=(N, W, E, S))
+text_preview.insert(INSERT, "No manifest loaded.")
+text_preview['state'] = 'disabled'
+
+preview_scroll = Scrollbar(preview_frame, command=text_preview.yview)
+preview_scroll.grid(column=1, row=0, sticky=(N, S))
+text_preview['yscrollcommand'] = preview_scroll.set
+
+for child in main_container.winfo_children():
+    child.grid_configure(padx=5, pady=5)
+
+
+root.mainloop()
